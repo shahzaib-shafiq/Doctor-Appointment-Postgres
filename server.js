38d9370e57b4fc8948c7e36d2cb2fe698f85d332
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2025-08-08 17:04:30 +0500

    Updated Server

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDb from './config/db.js';
-// import syncDb from './config/syncTables.js';
-import userRoutes from './routes/userRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
-import doctorRoutes from './routes/doctorRoutes.js';
-import departmentRoutes from './routes/departmentRoutes.js'
-import { sequelize } from './config/db.js';
-// import testRoutes from './routes/testRoutes.js';
 dotenv.config(); // Ensure environment variables are loaded
 // connectDb(); // Connect to MongoDB
 // syncDb();
@@ -28,22 +21,7 @@ await connectDb();
 console.log(colors.green('✅ PostgreSQL Table Sync!'));
 }
 
-Dbconnection();
-(async () => {
-  try {
-    await sequelize.sync({ alter: true });
-    console.log("✅ All tables synced successfully!");
-    process.exit();
-  } catch (error) {
-    console.error("❌ Error syncing tables:", error);
-    process.exit(1);
-  }
-})();
 //app.use('/api/v1/test', testRoutes);
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/admin', adminRoutes);
-app.use('/api/v1/doctor', doctorRoutes);
-app.use('/api/v1/department', departmentRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
